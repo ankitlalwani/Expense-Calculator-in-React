0ac51cfb34cf00ebd94628beb7c339a7c43a12a8
Ankit Lalwani <36794216+ankitlalwani@users.noreply.github.com>
2020-10-08 21:56:26 -0400

    Expense Tracker
    removing unwanted code

--- src/carousel.js
+++ src/carousel.js
@@ -1,8 +1,6 @@
 import React,{Component} from "react";
-import Carousel from 'react-bootstrap/Carousel'
 import "react-image-gallery/styles/css/image-gallery.css"
 import ImageGallery from 'react-image-gallery';
-import image1 from './assets/Expenses-01.jpg'
 import image2 from './assets/expenses-02.jpg'
 import image3 from './assets/expense-03.png'
 import image4 from './assets/expenses-04.jpg'
